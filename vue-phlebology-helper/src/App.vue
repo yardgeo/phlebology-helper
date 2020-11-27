4c3ce0e93ed981ec37aa3f7202e74eca355d5511
Gosha <gyarduhin@gmail.com>
2020-11-27 18:59:40 +0300

    add basic

---
+++ vue-phlebology-helper/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <dwvVue />
+  </div>
+</template>
+
+<script>
+  import dwvVue from './components/dwv'
+  export default {
+    name: 'app',
+    components: {
+      dwvVue
+    }
+  }
+</script>
+
+<style>
+  #app {
+    text-align: center;
+    height: 100vh;
+  }
+</style>
